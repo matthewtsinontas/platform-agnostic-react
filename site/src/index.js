45f5e2c31e45ceade99587ba0b3d4d9fef2cf66e
Matthew Tsinontas <matthewtsinontas@gmail.com>
2018-05-18 11:55:01 +0100

    Importing component accross

---
+++ site/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Title from 'title';
+
+ReactDOM.render(
+    <Title text="Site, not mobile" />,
+    document.getElementById('mount')
+);
