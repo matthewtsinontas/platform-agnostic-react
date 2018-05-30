f2d40222e9199678a30f3e2e88794f3912f47a1e
Matthew Tsinontas <matthewtsinontas@gmail.com>
2018-05-30 22:03:30 +0100

    Replacing site with create-react-app

--- site/src/index.js
+++ site/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Title from 'title';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <Title text="Site, not mobile" />,
-    document.getElementById('mount')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
